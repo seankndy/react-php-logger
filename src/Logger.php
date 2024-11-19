c1cc69e889efee6cce6e4effe53bfeac09730453
Sean Kennedy <sean@kndy.net>
2024-11-19 13:10:15 -0700

    upgrade to php8.1

--- src/Logger.php
+++ src/Logger.php
@@ -2,7 +2,6 @@
 namespace SeanKndy\ReactLogger;
 
 use React\Stream\WritableStreamInterface;
-use React\EventLoop\LoopInterface;
 use Psr\Log\AbstractLogger;
 use Psr\Log\LogLevel;
 use function WyriHaximus\PSR3\processPlaceHolders;
@@ -23,50 +22,40 @@ final class Logger extends AbstractLogger
         LogLevel::ALERT     => 550,
         LogLevel::EMERGENCY => 600
     ];
-    /**
-     * @var WritableStreamInterface
-     */
-    private $stream;
-    /**
-     * @var string
-     */
-    private $name;
-    /**
-     * @var int
-     */
-    private $minLevel;
 
-    public function __construct(string $name, WritableStreamInterface $stream, $minLevel = LogLevel::DEBUG)
-    {
-        $this->name = $name;
-        $this->stream = $stream;
-        $this->minLevel = \is_string($minLevel) ? self::LOG_LEVELS[$minLevel] : $minLevel;
+    private int $minLevel;
+
+    public function __construct(
+        private readonly string                  $name,
+        private readonly WritableStreamInterface $stream,
+        int|string                               $minLevel = LogLevel::DEBUG
+    ) {
+        $this->setMinLevel($minLevel);
     }
 
     /**
      * Set the minimum log level
-     *
-     * @var mixed $minLevel
-     *
-     * @return self
      */
-    public function setMinLevel($minLevel)
+    public function setMinLevel(int|string $minLevel): self
     {
-        $this->minLevel = $minLevel;
+        $this->minLevel = \is_string($minLevel) ? self::LOG_LEVELS[$minLevel] : $minLevel;
+
         return $this;
     }
 
     /**
      * {@inheritDoc}
      */
-    public function log($level, $message, array $context = []): void
+    public function log($level, string|\Stringable $message, array $context = []): void
     {
         if (!isset(self::LOG_LEVELS[$level]) || $this->minLevel > self::LOG_LEVELS[$level]) {
             return;
         }
+
         $message = '[' . \date('Y-m-d H:i:s') . '] ' . $this->name . '.' .
             \strtoupper($level) . ': ' . (string)$message;
         $message = processPlaceHolders($message, $context);
+
         $this->stream->write($message.\PHP_EOL);
     }
 }
